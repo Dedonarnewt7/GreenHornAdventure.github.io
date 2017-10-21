d6c9863c7d8a1b3dd02fc181f589ab469be9da8b
Dedonarnewt7 <Dedonarnewt7@gmail.com>
2017-10-20 21:14:11 -0500

    OCT 20 2017
    update

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -37,3 +37,24 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 900, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
